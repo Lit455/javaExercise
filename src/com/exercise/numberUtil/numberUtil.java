4e9408be35a7867d910626ca550b309c9e80d502
Cxy <waltcao93@gmail.com>
2016-09-13 13:53:11 +0800

    2016-9-13 numberUtil

--- src/com/exercise/numberUtil/numberUtil.java
+++ src/com/exercise/numberUtil/numberUtil.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 /**
  * Created by Fyol93 on 2016/9/12.
  * 排序
@@ -17,14 +19,24 @@ public class numberUtil {
             num.add(scanner.nextInt());
         }
         int exchange;
-        for (int i = 1; i < num.size(); i++) {
-            //for(int j)
-            if (num.get(num.size() - i) < num.get(num.size() - i - 1)) {
-                exchange = num.get(num.size() - i);
-                num.set(num.size() - i, num.get(num.size() - i - 1));
-                num.set(num.size() - i - 1, exchange);
+//两个循环嵌套，从尾部开始两两判断后一个数是否比前一个数大，否的话交换两个数
+        for(int k = 1; k < num.size(); k++)
+            for (int i = 1; i < num.size(); i++) {
+                if (num.get(num.size() - i) < num.get(num.size() - i - 1)) {
+                    exchange = num.get(num.size() - i);
+                    num.set(num.size() - i, num.get(num.size() - i - 1));
+                    num.set(num.size() - i - 1, exchange);
+                }
+                else continue;
             }
-        }
+//        System.out.println(num);
+//        for(int j = 1; j < num.size(); j++)
+//            if(num.get(num.size()-1) < num.get(j-1))
+//                for(int k = 1; k <= (num.size() - j); k++){
+//                    exchange = num.get(num.size() - k);
+//                    num.set(num.size() - k, num.get(num.size() - k - 1));
+//                    num.set(num.size() - k - 1, exchange);
+//                }
         System.out.println(num);
     }
 
